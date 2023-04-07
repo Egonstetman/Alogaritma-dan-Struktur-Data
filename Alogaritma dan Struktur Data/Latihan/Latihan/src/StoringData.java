01fd8e8c8d9a05fd81e13c1fc91deddb1e187032
Egonstetman <106148025+Egonstetman@users.noreply.github.com>
2023-04-07 07:47:49 -0700

    Add files via upload

---
+++ Alogaritma dan Struktur Data/Latihan/Latihan/src/StoringData.java
@@ -0,0 +1,14 @@
+public class StoringData {
+    public static void main(String[] args) throws Exception {
+               
+        //store string value
+        String name = "James";
+
+        //store boolean value
+        boolean decision = true;
+
+        //store number value
+        int score = 100
+
+    }
+}
